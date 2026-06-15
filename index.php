989cbfd7cac5ad2eee66633f583f632b3373f9ec
Aislan <aislan.ss29@gmail.com>
2026-06-14 21:56:58 -0300

    segundo commit

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API Alunos</title>
+</head>
+<body>
+
+</body>
+</html>
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
